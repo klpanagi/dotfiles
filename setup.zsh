a2599ef98861e39ca61570859d45653699d4f0bc
Konstantinos Panayiotou <klpanagi@gmail.com>
2016-02-23 17:28:35 +0200

    Clean before setup

--- setup.zsh
+++ setup.zsh
@@ -2,6 +2,12 @@
 
 local filepath=`dirname ${0:A}`
 
+local tmuxConfFile="${HOME}/.tmux.conf"
+local urxvtConfFile="${HOME}/.Xdefaults"
+
+`rm "${tmuxConfFile}"`
+`rm "${urxvtConfFile}"`
+
 # Tmux
 `ln -s ${filepath}/tmux/tmux.conf "${HOME}/.tmux.conf"`
 # urxvt
