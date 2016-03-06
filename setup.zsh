677fb7f40448d9b015d1f3669258e10ec2e7932b
Konstantinos Panayiotou <klpanagi@gmail.com>
2016-03-06 17:44:09 +0200

    .

--- setup.zsh
+++ setup.zsh
@@ -4,11 +4,14 @@ local filepath=`dirname ${0:A}`
 
 local tmuxConfFile="${HOME}/.tmux.conf"
 local urxvtConfFile="${HOME}/.Xdefaults"
+local keymapConfFile="${HOME}/.inputrc"
 
 `rm "${tmuxConfFile}"`
 `rm "${urxvtConfFile}"`
+`rm "${keymapConfFile}"`
 
 # Tmux
-`ln -s ${filepath}/tmux/tmux.conf "${HOME}/.tmux.conf"`
+`ln -s ${filepath}/tmux/tmux.conf "${tmuxConfFile}"`
 # urxvt
-`ln -s ${filepath}/urxvt/Xdefaults.urxvt.default "${HOME}/.Xdefaults"`
+`ln -s ${filepath}/urxvt/Xdefaults.urxvt.default "${urxvtConfFile}"`
+`ln -s ${filepath}/urxvt/inputrc "${keymapConfFile}"`
