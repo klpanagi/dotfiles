55b7c3125c03357848d53e99e0d519ed4677e421
Konstantinos Panayiotou <klpanagi@gmail.com>
2016-04-17 16:27:56 +0300

    xinitrc file

--- setup.zsh
+++ setup.zsh
@@ -5,13 +5,20 @@ local filepath=`dirname ${0:A}`
 local tmuxConfFile="${HOME}/.tmux.conf"
 local urxvtConfFile="${HOME}/.Xdefaults"
 local keymapConfFile="${HOME}/.inputrc"
+local terminatorConfFile="${HOME}/.config/terminator/config"
+local xinitFile="${HOME}/.xinitrc"
 
 `rm "${tmuxConfFile}"`
 `rm "${urxvtConfFile}"`
 `rm "${keymapConfFile}"`
+`rm "${terminatorConfFile}"`
+`rm "${xinitFile}"`
 
 # Tmux
 `ln -s ${filepath}/tmux/tmux.conf "${tmuxConfFile}"`
 # urxvt
 `ln -s ${filepath}/urxvt/Xdefaults.urxvt.default "${urxvtConfFile}"`
 `ln -s ${filepath}/urxvt/inputrc "${keymapConfFile}"`
+# Terminator
+`ln -s ${filepath/terminator/config} ${terminatorConfFile}`
+`ln -s ${filepath/x/xinitrc} ${xinitFile}`
