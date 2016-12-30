401437c3329cce6fc73902c027279ea79f5ebf84
Konstantinos Panayiotou <klpanagi@gmail.com>
2016-12-30 21:21:07 +0200

    Git-config file

--- setup.zsh
+++ setup.zsh
@@ -1,4 +1,4 @@
-#!/usr/bin/zsh
+#!/bin/sh
 
 local filepath=`dirname ${0:A}`
 
@@ -7,18 +7,22 @@ local urxvtConfFile="${HOME}/.Xdefaults"
 local keymapConfFile="${HOME}/.inputrc"
 local terminatorConfFile="${HOME}/.config/terminator/config"
 local xinitFile="${HOME}/.xinitrc"
+local gitConfig="${HOME}/.gitconfig"
 
-`rm "${tmuxConfFile}"`
-`rm "${urxvtConfFile}"`
-`rm "${keymapConfFile}"`
-`rm "${terminatorConfFile}"`
-`rm "${xinitFile}"`
+rm "${tmuxConfFile}" &>/dev/null
+rm "${urxvtConfFile}" &>/dev/null
+rm "${keymapConfFile}" &>/dev/null
+rm "${terminatorConfFile}" &>/dev/null
+rm "${xinitFile}" &>/dev/null
 
 # Tmux
-`ln -s ${filepath}/tmux/tmux.conf "${tmuxConfFile}"`
+ln -s ${filepath}/tmux/tmux.conf "${tmuxConfFile}"
 # urxvt
-`ln -s ${filepath}/urxvt/Xdefaults.urxvt.default "${urxvtConfFile}"`
-`ln -s ${filepath}/urxvt/inputrc "${keymapConfFile}"`
+ln -s ${filepath}/urxvt/Xdefaults.urxvt.default "${urxvtConfFile}"
+ln -s ${filepath}/urxvt/inputrc "${keymapConfFile}"
 # Terminator
-`ln -s ${filepath}/terminator/config ${terminatorConfFile}`
-`ln -s ${filepath}/x/xinitrc ${xinitFile}`
+ln -s ${filepath}/terminator/config ${terminatorConfFile}
+ln -s ${filepath}/x/xinitrc ${xinitFile}
+
+ln -s ${filepath}/gitconfig ${gitConfig}
+
