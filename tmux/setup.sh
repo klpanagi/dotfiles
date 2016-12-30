e54dafe9eb819d5cbd673369c1dc860279dc462f
Konstantinos Panayiotou <klpanagi@gmail.com>
2016-12-30 23:02:07 +0200

    .

--- tmux/setup.sh
+++ tmux/setup.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env sh
 
-# Dependencies {
-  # libevent2-dev
-  # libncurses-dev
-#}
 
-git clone https://github.com/tmux/tmux.git
-cd tmux
-sh autogen.sh
-./configure && make
-sudo make install
-cd ..
-rm -rf tmux/
+echo -e '\033[1;33mInstalling tmux from repo: tmux/tmux.git\033[0m'
+sleep 1
+cd /tmp && git clone https://github.com/tmux/tmux.git && cd tmux && \
+  sh autogen.sh && ./configure && make && sudo make install
+
+echo -e '\033[1;33mInstalling tmux package manager (tpm) from repo: tmux/tmux.git\033[0m'
+sleep 1
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+tmux source ~/.tmux.conf
+
+# Send tmux install keystroke
+xdotool key control+a I
