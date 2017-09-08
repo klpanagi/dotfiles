798d8299e92ad7baf0c27b86faf7b58a58cc614e
Konstantinos Panayiotou <klpanagi@gmail.com>
2017-09-09 00:48:18 +0300

    Alias in zsh_aliases file

---
+++ zsh/install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+[[ -f ~/.zshrc ]] && mv ~/.zshrc ~/.zshrc.bak
+[[ -f ~/.zsh_aliases ]] && mv ~/.zsh_aliases ~/.zsh_aliases.bak
+[[ -f ~/.zsh_functions ]] &&mv ~/.zsh_functions ~/.zsh_functions.bak
+ln -s `pwd`/zshrc ~/.zshrc
+ln -s `pwd`/zsh_functions ~/.zsh_aliases
+ln -s `pwd`/zsh_functions ~/.zsh_functions
