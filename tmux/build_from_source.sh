2431ebc21984b8ef7836f888a52aa1346ce797d3
Konstantinos Panayiotou <klpanagi@gmail.com>
2018-01-07 03:59:41 +0200

    tmux build from source

---
+++ tmux/build_from_source.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+
+echo -e '\033[1;33mInstalling tmux from repo: tmux/tmux.git\033[0m'
+sleep 1
+cd /tmp && git clone https://github.com/tmux/tmux.git && cd tmux && \
+  sh autogen.sh && ./configure && make && sudo make install
+
+echo -e '\033[1;33mInstalling tmux package manager (tpm) from repo: tmux/tmux.git\033[0m'
+sleep 1
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
--- tmux/install.sh
+++ tmux/install.sh
@@ -1,13 +1,10 @@
 #!/usr/bin/env sh
 
-echo -e '\033[1;33mInstalling tmux from repo: tmux/tmux.git\033[0m'
-sleep 1
-cd /tmp && git clone https://github.com/tmux/tmux.git && cd tmux && \
-  sh autogen.sh && ./configure && make && sudo make install
+if [ -f "~/.tmux.conf" ]; then
+  mv ~/.tmux.conf ~/.tmux.conf.bak
+fi
+ln -s $(pwd)/tmux.conf ~/.tmux.conf
 
-echo -e '\033[1;33mInstalling tmux package manager (tpm) from repo: tmux/tmux.git\033[0m'
-sleep 1
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 tmux source ~/.tmux.conf
 
 # Send tmux install keystroke
