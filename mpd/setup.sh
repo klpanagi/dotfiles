85caee4f90e5a4659508b4022e0d66e8a72df68e
Konstantinos Panayiotou <klpanagi@gmail.com>
2018-05-27 22:17:50 +0300

    MPD server-side configuration

---
+++ mpd/setup.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+echo -e "[*] - Creating parent config directory: ~/.config/mpd"
+mkdir -p ~/.config/mpd
+
+echo -e "[*] - Creating directory for playlists: ~/.config/mpd/playlists"
+mkdir -p ~/.config/mpd/playlists
+
